015550340c10d9e56f7e06a24c66c97783c2bb27
ratanturbe-blip <ratanturbe@gmail.com>
2026-07-06 09:26:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
-function searchProduct() {
-
-let input = document.getElementById("searchBox").value;
-
-if(input==""){
-alert("Please enter a product name");
-return;
-}
-
-window.open("https://www.amazon.in/s?k="+encodeURIComponent(input), "_blank");
-
-}
+console.log("Hub Guru Loaded Successfully");
